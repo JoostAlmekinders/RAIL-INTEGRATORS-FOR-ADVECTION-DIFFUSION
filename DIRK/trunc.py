c4c6a4cc9880611b26089d00e489ea983d05a4b5
JoostAlmekinders <154852716+JoostAlmekinders@users.noreply.github.com>
2025-08-21 18:40:26 -0400

    Create trunc.py

---
+++ DIRK/trunc.py
@@ -0,0 +1,29 @@
+"""
+Truncation Function 
+
+Author: Joost Almekinders
+Date: July 4, 2025
+
+"""
+
+import numpy as np
+import scipy.linalg
+
+import math 
+
+
+def trunc(Vx_nn,S_nn,Vy_nn,tol):
+    U,S, VT = np.linalg.svd(S_nn, full_matrices=False)
+    r = np.sum(S > tol * S[0]) 
+    if r == 0:
+        r = 1
+    
+
+    Vx_nn  = Vx_nn@ U[:,:r]
+    #Vy_nn = Vy_nn@ VT[:,:r]
+    Vy_nn = Vy_nn@ VT[:r,:].T
+    S_nn = np.diag(S[:r])
+    r_nn = r
+   
+
+    return Vx_nn, S_nn, Vy_nn,r_nn
