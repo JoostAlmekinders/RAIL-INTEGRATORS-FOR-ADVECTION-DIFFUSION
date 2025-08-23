f7df45b269ec3ee3b4cf46362e06ebe83f7a1651
JoostAlmekinders <154852716+JoostAlmekinders@users.noreply.github.com>
2025-08-23 17:29:57 -0400

    Update addmat.py

--- DIRK/addmat.py
+++ DIRK/addmat.py
@@ -16,12 +16,8 @@ from scipy.linalg import qr, svd, block_diag
 
 def addmat(Vx_1,S_1,Vy_1,Vx_2,S_2,Vy_2):
     
-    # Vxtot = np.hstack((Vx_1, Vx_2))
-    # Vytot = np.hstack((Vy_1, Vy_2))
-
-    # Qx,Rx = np.linalg.qr(Vxtot, mode='economic')
-    # Qy,Ry = np.linalg.qr(Vytot, mode='economic')
-
+   
+    ## compine both Vx's and Vy's and perfom qr decomposition
     Qx, Rx = qr(np.hstack([Vx_1, Vx_2]), mode='economic')
     Qy, Ry = qr(np.hstack([Vy_1, Vy_2]), mode='economic')
 
@@ -31,7 +27,7 @@ def addmat(Vx_1,S_1,Vy_1,Vx_2,S_2,Vy_2):
 
     U,S,V = np.linalg.svd(val, full_matrices=False)
 
-    #r = np.max(np.where(np.diag(S) > 1.0e-12)[0]) + 1 if np.any(np.diag(S) > 1.0e-12) else 0
+    ##cut based off tolerance 
     tol = 1e-12
     r = np.sum(S > tol)
     if r == 0:
@@ -40,5 +36,5 @@ def addmat(Vx_1,S_1,Vy_1,Vx_2,S_2,Vy_2):
     Vx = Qx@ U[:,:r] 
     S = np.diag(S[:r])
     Vy = Qy@ V[:r, :].T
-    #Vy = Qy@ V[:,:r]
+    
     return Vx, S, Vy
