9fbd7a40d720d3f72560af32f4f258df4bb40801
JoostAlmekinders <154852716+JoostAlmekinders@users.noreply.github.com>
2025-08-24 11:40:41 -0400

    Update IMEX1_fxn.py

--- IMEX/IMEX1_fxn.py
+++ IMEX/IMEX1_fxn.py
@@ -1,6 +1,5 @@
 """
 
-
 IMEX (1,1,1) Functions 
 Author: Joost Almekinders
 Date: August 5, 2025 
@@ -23,8 +22,7 @@ from red_aug_gen import redaug_g
 
 
 def IMEX111(Dxx, Dyy, Dx, Dy, tol, dtn,Vx_n,S_n,Vy_n,r_n,Gx,Gxy,Gy,a1,a2,b1,b2,c1,c2,Nx,Ny,tn):
-
-            #Vx_star, Vy_star, _ = redaug_2(np.empty((Vx_n.shape[0], 0)), Vx_n, np.empty((Vy_n.shape[0], 0)), Vy_n)
+            
             Vx_star, Vy_star, _ = redaug_g([np.empty((Vx_n.shape[0], 0)),Vx_n], [np.empty((Vy_n.shape[0], 0)), Vy_n], 1.0e-12)
             
             tempx, temps, tempy = addmat(Vx_n, S_n, Vy_n, Dx @ (a1[:, np.newaxis] * Vx_n),-dtn*b1(tn) *S_n, c1[:, np.newaxis]*Vy_n)
@@ -32,20 +30,22 @@ def IMEX111(Dxx, Dyy, Dx, Dy, tol, dtn,Vx_n,S_n,Vy_n,r_n,Gx,Gxy,Gy,a1,a2,b1,b2,c
             
             Wx, Ws, Wy = addmat(tempx2, temps2, tempy2, Gx, dtn*Gxy(tn + dtn), Gy)
 
-            
+            #k-step
             K = solve_sylvester(np.eye(Nx) - dtn*Dxx, -dtn *(Dyy@Vy_star).T@Vy_star,  Wx@Ws@(Wy.T@Vy_star))
             Vx_nn, _ = np.linalg.qr(K, mode= 'reduced')
 
+            #L-step
             L = solve_sylvester(np.eye(Ny) - dtn*Dyy, -dtn *(Dxx@Vx_star).T @Vx_star, Wy@Ws.T@(Wx.T@ Vx_star))
             Vy_nn, _ = np.linalg.qr(L, mode='reduced')
 
-            #Vx_nn, Vy_nn, R = redaug_2(Vx_nn,Vx_n,Vy_nn, Vy_n)
+            #reduced augmentation 
             Vx_nn, Vy_nn, R = redaug_g([Vx_nn,Vx_n], [Vy_nn, Vy_n], 1.0e-12)
 
+            #S-step
             S_nn = solve_sylvester(np.eye(R) - dtn* Vx_nn.T@ (Dxx@ Vx_nn), -dtn* (Dyy@Vy_nn).T @ Vy_nn, (Vx_nn.T@Wx)@Ws@(Wy.T @ Vy_nn ))
 
 
-
+            #final truncation 
             Vx_nn, S_nn, Vy_nn, r_nn = trunc(Vx_nn,S_nn,Vy_nn,tol)
 
             return Vx_nn, S_nn, Vy_nn, r_nn
