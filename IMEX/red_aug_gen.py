298649d061020fb30e1f7995ded6ba2eb2da1b2f
JoostAlmekinders <154852716+JoostAlmekinders@users.noreply.github.com>
2025-08-24 11:47:31 -0400

    Update red_aug_gen.py

--- IMEX/red_aug_gen.py
+++ IMEX/red_aug_gen.py
@@ -2,12 +2,11 @@
 import numpy as np
 def redaug_g(Vx_list, Vy_list, tol):
     
-    #Vx_comb = np.hstack(Vx_list)
+
     Qx, Rx = np.linalg.qr(np.hstack(Vx_list), mode='reduced')
     Vx_temp, S_tempx, _ = np.linalg.svd(Rx, full_matrices=False)
     rx = np.sum(S_tempx > tol * S_tempx[0])
-    
-    #Vy_comb = np.hstack(Vy_list)
+
     Qy, Ry = np.linalg.qr(np.hstack(Vy_list), mode='reduced')
     Vy_temp, S_tempy, _ = np.linalg.svd(Ry, full_matrices=False)
     ry = np.sum(S_tempy > tol * S_tempy[0])
